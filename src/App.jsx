b5ee98a405b2b5b2f35b037195d77031ef9afc1c
Khushi Kumawat <lilakumawat30@gmail.com>
2025-12-03 16:18:13 +0530

    diceReact

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import dice1 from "./assets/dice1.jpg"
+import dice2 from "./assets/dice2.jpg"
+import dice3 from "./assets/dice3.jpg"
+import dice4 from "./assets/dice4.jpg"
+import dice5 from "./assets/dice5.jpg"
+import dice6 from "./assets/dice6.jpg"
+import question from "./assets/question.jpg"
+
+const App = () => {
+  const images = [dice1, dice2, dice3, dice4, dice5, dice6]
+
+  const [currentImage, setCurrentImage] = useState(question)
+  const [score, setScore] = useState(0)
+  const [rolledNumbers, setRolledNumbers] = useState([]) 
+
+  function rollDice() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    setCurrentImage(images[randomNumber - 1])
+    setScore(oldScore => oldScore + randomNumber)
+    setRolledNumbers(prev => [...prev, randomNumber]) 
+  }
+
+  const numbers = []
+  for (let i = 1; i <= 100; i++) {
+    numbers.push(
+      <p key={i} className={rolledNumbers.includes(i) ? 'highlight' : ''} >  {i}</p>
+    )
+  }
+
+  return (
+    <>
+      <h2>Score: {score}</h2>
+
+      <div className='diceNumber'>
+        {numbers}
+      </div>
+
+      <img src={currentImage} alt="Dice" />
+
+      <button className='diceBtn' onClick={rollDice}>Click Here</button>
+    </>
+  )
+}
+
+export default App
